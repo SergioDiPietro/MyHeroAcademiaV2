88dbdcb5f05419c58c3e25d6b9e1109608f210b4
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2021-12-02 22:24:14 +0000

    Initial commit: Creación de proyecto en base a su predecesor, myHeroAcademia (V1)

---
+++ app/src/main/java/com/example/myheroacademiav2/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.myheroacademiav2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.myheroacademiav2.utils.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText heroIdBox;
+    TextView urlDisplay;
+    TextView resultDisplay;
+
+    public class HeroesQueryTask extends AsyncTask<URL, Void, String> {
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String heroSearchResolve = null;
+
+            try {
+                heroSearchResolve = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return heroSearchResolve;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")) {
+                resultDisplay.setText(s);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.launch) {
+            URL heroesApiUrl;
+            if (heroIdBox.getText().toString().isEmpty()) {
+                heroesApiUrl = NetworkUtils.generateUrl();
+            } else {
+                heroesApiUrl = NetworkUtils.generateUrl(heroIdBox.getText().toString());
+            }
+            urlDisplay.setText(heroesApiUrl.toString());
+
+            new HeroesQueryTask().execute(heroesApiUrl);
+        } else if (itemId == R.id.clear) {
+            urlDisplay.setText("");
+            resultDisplay.setText("");
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        heroIdBox = (EditText)findViewById(R.id.hero_id);
+        urlDisplay = (TextView) findViewById(R.id.url_display);
+        resultDisplay = (TextView) findViewById(R.id.result_display);
+    }
+}
---
+++ app/src/main/java/com/example/myheroacademiav2/utils/NetworkUtils.java
@@ -0,0 +1,60 @@
+package com.example.myheroacademiav2.utils;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    final static String BASE_URL = "https://akabab.github.io/superhero-api/api";
+    final static String ALL_HEROES_PARAM = "all";
+    final static String SINGLE_HERO_PARAM = "id";
+    final static String URL_END = ".json";
+
+    public static URL generateUrl() {
+        Uri buildUri = Uri.withAppendedPath(Uri.parse(BASE_URL), ALL_HEROES_PARAM + URL_END)
+                .buildUpon().build();
+
+        return UriToURL(buildUri);
+    }
+
+    public static URL generateUrl(String heroID) {
+        Uri buildUri = Uri
+                .withAppendedPath(Uri.parse(BASE_URL), SINGLE_HERO_PARAM)
+                .buildUpon().build();
+
+        Uri buildUri2 = Uri.withAppendedPath(buildUri, heroID + URL_END);
+
+        return UriToURL(buildUri2);
+    }
+
+    private static URL UriToURL(Uri uri) {
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        InputStream is = urlConnection.getInputStream();
+
+        Scanner scan = new Scanner(is);
+        scan.useDelimiter("\\A");
+        try {
+            if (scan.hasNext()) {
+                return scan.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
